7b3e8bd1ae1f2d7602542f22b0522404b5bcf5bd
asharykin <aleksandr-sharykin@mail.ru>
2025-01-19 23:11:08 +0300

    Minor change

--- monitor_test.sh
+++ monitor_test.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-PROCESS_NAME="test"
 URL="https://test.com/monitoring/test/api"
 PID_FILE="/tmp/test_process.pid"
 LOG_FILE="/var/log/monitoring.log"
 
 log() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
+    echo "$(date) - $1" >> $LOG_FILE
 }
 
-if pgrep "$PROCESS_NAME" > /dev/null; then
-    CURRENT_PID=$(pgrep "$PROCESS_NAME")
+if pgrep test > /dev/null; then
+    CURRENT_PID=$(pgrep test)
 
-    if [ ! -f "$PID_FILE" ] || [ "$(cat "$PID_FILE")" != "$CURRENT_PID" ]; then
-        log "Процесс '$PROCESS_NAME' был перезапущен. Новый PID: $CURRENT_PID"
-        echo "$CURRENT_PID" > "$PID_FILE"
+    if [ ! -f $PID_FILE ] || [ "$(cat $PID_FILE)" != $CURRENT_PID ]; then
+        log "Процесс 'test' был перезапущен. Новый PID: $CURRENT_PID"
+        echo $CURRENT_PID > $PID_FILE
     fi
 
     if ! curl -s --head "$URL" | grep "200 OK" > /dev/null; then
